09e545a9efc63ecb8652f4175bc7ca3ee5579ccb
John Edwin Floyd <johnedwinfloyd@Johns-MacBook.local>
2017-11-07 16:23:47 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,5 +1,26 @@
+var cards = [
+  {
+    card: 'queen',
+    suit: 'hearts',
+    cardImage: 'images/queen-of-hearts.png'
+  },
+  {
+    card: 'queen',
+    suit: 'diamonds',
+    cardImage: 'images/queen-of-diamonds.png'
+  },
+  {
+    card: 'king',
+    suit: 'hearts',
+    cardImage: 'images/king-of-hearts.png'
+  },
+  {
+    card: 'king',
+    suit: 'diamonds',
+    cardImage: 'images/king-of-diamonds.png'
+  }
+];
 
-var cards = ['queen', 'queen', 'king', 'king'];
 var cardsInPlay = [];
 
 var checkForMatch = function () {
@@ -11,13 +32,20 @@ var checkForMatch = function () {
 };
 
 var flipCard = function (cardId) {
-  console.log("User flipped" + cards[cardId]);
-  cardsInPlay.push(cards[cardId]);
+
+  console.log("User flipped " + cards[cardId].card);
+
+  cardsInPlay.push(cards[cardId].card);
+
+  console.log(cards[cardId].cardImage)
+
+  console.log(cards[cardId].suit)
+
   if (cardsInPlay.length === 2) {
     checkForMatch();
+    cardsInPlay = [];
   }
 };
-
 
 flipCard(0);
 flipCard(2);
